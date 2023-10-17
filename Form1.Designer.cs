4a3f2037ba5302876b4cdf22505a8319b5f64b1e
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-17 13:22:16 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Students
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNxt = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dbStudentsDataSet4 = new Students.DbStudentsDataSet4();
+            this.tbStudentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbStudentTableAdapter = new Students.DbStudentsDataSet4TableAdapters.tbStudentTableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stuNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stuPhoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stuAgeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbStudentsDataSet4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbStudentBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Phone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student Age";
+            // 
+            // txtName
+            // 
+            this.txtName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tbStudentBindingSource, "Stu_Name", true));
+            this.txtName.Location = new System.Drawing.Point(124, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(169, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tbStudentBindingSource, "Stu_Phone", true));
+            this.txtPhone.Location = new System.Drawing.Point(124, 103);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(126, 22);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtAge
+            // 
+            this.txtAge.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tbStudentBindingSource, "Stu_Age", true));
+            this.txtAge.Location = new System.Drawing.Point(124, 146);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 22);
+            this.txtAge.TabIndex = 5;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(29, 272);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 6;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(124, 272);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(218, 272);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(312, 272);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(29, 332);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 10;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(124, 332);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(218, 332);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 12;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNxt
+            // 
+            this.btnNxt.Location = new System.Drawing.Point(312, 332);
+            this.btnNxt.Name = "btnNxt";
+            this.btnNxt.Size = new System.Drawing.Size(75, 23);
+            this.btnNxt.TabIndex = 13;
+            this.btnNxt.Text = "Next";
+            this.btnNxt.UseVisualStyleBackColor = true;
+            this.btnNxt.Click += new System.EventHandler(this.btnNxt_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.stuNameDataGridViewTextBoxColumn,
+            this.stuPhoneDataGridViewTextBoxColumn,
+            this.stuAgeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tbStudentBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(394, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(686, 150);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // dbStudentsDataSet4
+            // 
+            this.dbStudentsDataSet4.DataSetName = "DbStudentsDataSet4";
+            this.dbStudentsDataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbStudentBindingSource
+            // 
+            this.tbStudentBindingSource.DataMember = "tbStudent";
+            this.tbStudentBindingSource.DataSource = this.dbStudentsDataSet4;
+            // 
+            // tbStudentTableAdapter
+            // 
+            this.tbStudentTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // stuNameDataGridViewTextBoxColumn
+            // 
+            this.stuNameDataGridViewTextBoxColumn.DataPropertyName = "Stu_Name";
+            this.stuNameDataGridViewTextBoxColumn.HeaderText = "Stu_Name";
+            this.stuNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.stuNameDataGridViewTextBoxColumn.Name = "stuNameDataGridViewTextBoxColumn";
+            // 
+            // stuPhoneDataGridViewTextBoxColumn
+            // 
+            this.stuPhoneDataGridViewTextBoxColumn.DataPropertyName = "Stu_Phone";
+            this.stuPhoneDataGridViewTextBoxColumn.HeaderText = "Stu_Phone";
+            this.stuPhoneDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.stuPhoneDataGridViewTextBoxColumn.Name = "stuPhoneDataGridViewTextBoxColumn";
+            // 
+            // stuAgeDataGridViewTextBoxColumn
+            // 
+            this.stuAgeDataGridViewTextBoxColumn.DataPropertyName = "Stu_Age";
+            this.stuAgeDataGridViewTextBoxColumn.HeaderText = "Stu_Age";
+            this.stuAgeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.stuAgeDataGridViewTextBoxColumn.Name = "stuAgeDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnNxt);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Students";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbStudentsDataSet4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbStudentBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNxt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DbStudentsDataSet4 dbStudentsDataSet4;
+        private System.Windows.Forms.BindingSource tbStudentBindingSource;
+        private DbStudentsDataSet4TableAdapters.tbStudentTableAdapter tbStudentTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stuNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stuPhoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stuAgeDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Students
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.tbStudentTableAdapter.Fill(this.dbStudentsDataSet4.tbStudent);
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            tbStudentBindingSource.AddNew();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+           tbStudentBindingSource.EndEdit();
+            tbStudentTableAdapter.Update(dbStudentsDataSet4.tbStudent);
+           
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            tbStudentBindingSource.RemoveCurrent();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+           tbStudentBindingSource.MoveFirst();
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            tbStudentBindingSource.MoveFirst();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            tbStudentBindingSource.MovePrevious(); 
+        }
+
+        private void btnNxt_Click(object sender, EventArgs e)
+        {
+          tbStudentBindingSource.MoveNext();
+        }
+    }
+}
